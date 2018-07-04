d453cf072deebeb46e54140bc707b5c31b051532
Bharat <bharatdarawde1134@gmail.com>
2018-07-04 06:54:23 +0530

    FirstCommit

---
+++ HelloWorld/app/src/main/java/com/example/bharatd/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bharatd.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
